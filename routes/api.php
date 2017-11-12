09176b3417dd40b830cdaac0df0187e3dc73190e
pyounuskhan <pyounuskhan@gmail.com>
2017-11-12 10:58:58 +0530

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/products','Productcontroller');
+Route::group(['prefix'=>'products'],function(){
+	Route::apiResource('/{product}/reviews','ReviewController');
+});
